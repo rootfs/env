da6a68e27a0c4ef4ada34c70c6ea0faf541e143b
Huamin Chen <hchen@redhat.com>
2016-02-07 00:06:23 +0000

    yum install vagrant and virtualbox
    Signed-off-by: Huamin Chen <hchen@redhat.com>

---
+++ install-vb.sh
@@ -0,0 +1,3 @@
+yum install -y "kernel-devel-uname-r == $(uname -r)"
+yum install -y http://download.virtualbox.org/virtualbox/5.0.14/VirtualBox-5.0-5.0.14_105127_fedora22-1.x86_64.rpm
+yum install -y https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.rpm
